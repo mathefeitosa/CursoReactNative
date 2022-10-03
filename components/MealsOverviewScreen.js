f0de15be2c5d6c3bf29b7a02598002223f54bce9
Matheus Feitosa <mathe.feitosa@gmail.com>
2022-10-03 11:40:32 -0300

    010 displaying meals

--- components/MealsOverviewScreen.js
+++ components/MealsOverviewScreen.js
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "./MealItem";
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(data) {
+    return <MealItem title={data.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
