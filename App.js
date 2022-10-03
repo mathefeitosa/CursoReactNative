87f75a8b424821f4ae4e9792a020a734475c8f96
Matheus Feitosa <mathe.feitosa@gmail.com>
2022-10-03 11:18:17 -0300

    006 implementing navigation between two screens

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar, StyleSheet } from "react-native";
+import MealsOverviewScreen from "./components/MealsOverviewScreen";
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,28 @@
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
+
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(data) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview");
+    }
+    return (
+      <CategoryGridTile
+        title={data.item.title}
+        color={data.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
